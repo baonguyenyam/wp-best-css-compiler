f91b575b813428edbe744a49dd7719aceec93a51
Nguyen Pham <baonguyenyam@gmail.com>
2021-08-23 20:21:03 -0500

    Update

--- nguyen-app.php
+++ nguyen-app.php
@@ -7,7 +7,7 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Best CSS Compiler
- * Plugin URI:        https://wordpress.org/plugins/best-css-compiler
+ * Plugin URI:        https://wow-wp.com
  * Description:       Best CSS Compiler is a CSS preprocessor, a superset of CSS that puts in features that aren’t functional in regular CSS. Best CSS Compiler puts in features to CSS and gets collected into legal CSS
  * Version:           1.0.0
  * Author:            Nguyen Pham
